180aa998dcfc10f16a273533c4bf3934499ec1a0
Mahmudur Rahman Salman <mahmudur.salman@gmail.com>
2026-04-17 19:56:34 +0600

    first commit

---
+++ frontend/src/routes/router.jsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router";
+import RootLayouts from "../layouts/RootLayouts";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayouts></RootLayouts>
+  },
+]);
